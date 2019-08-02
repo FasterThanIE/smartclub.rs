5b798826665a728b974d615aa79841812b4891ab
Localhost user <tomicawarr@gmail.com>
2019-08-02 21:56:31 +0200

    travel insurance finished all

--- resources/views/partials/travel_insurance.blade.php
+++ resources/views/partials/travel_insurance.blade.php
@@ -13,9 +13,9 @@
     <p>Putno osiguranje je jedno od najrasprostranjenijih osiguranja koje se ugovara u Srbiji. Odnos pokrića i cene je u korist klijenta pa tako za manje od 100 RSD po danu,
         klijent može da dobije zaštitu u slučaju nemilih događaja u inostranstvu</p>
     <p>Putno osiguranje pokriva sledeće rizike:</p>
-    <ul class="listNone">
-        <li>- Troškovi zdravstvene zaštite</li>
-        <li>- Gubitak prtljaga – ugovara se posebno</li>
+    <ul class="listNone p-1 p-lg-2">
+        <li class="p-2">- Troškovi zdravstvene zaštite</li>
+        <li class="p-2">- Gubitak prtljaga – ugovara se posebno</li>
     </ul>
 
     <h4 class="p-2">Polisa osiguranja može biti:</h4>
@@ -35,6 +35,23 @@
     <h4 class="p-2">4. Multi polisa</h4>
     <p>U ponudi su i putovanja sa više izlazaka iz zemlje na period od 15, 30, 50, 60, 90 i 180 dana tokom jedne godine.
         Ugovoreni broj dana osiguravajućeg pokrića može se iskoristiti u kontinuitetu ili sa više ulazaka/izlazaka iz zemlje.</p>
+
+    <p>Zašto ugovoriti putno osiguranje?</p>
+    <ul class="listNone p-1 p-lg-2">
+        <li class="p-2">- Organizacija pomoći i naknada troškova lečenja u inostranstvu koji mogu biti izuzetno visoki.</li>
+        <li class="p-2">- Dežurni call centar Vam je na raspolaganju 24 časa dnevno svakog dana.</li>
+        <li class="p-2">- Medicinska, putna i pravna asistencija.</li>
+        <li class="p-2">- I najmanja gripa, koja se leči u inostranstvu, može da košta preko 100 EUR.</li>
+    </ul>
+
+    <p>Na šta obratiti pažnju?</p>
+    <ul class="listNone p-1 p-lg-2">
+        <li class="p-2">- Mreža asistentskih ustanova u zemlji boravka.</li>
+        <li class="p-2">- Raspitajte se, šta je sve pokriveno Vašim putnim osiguranjem.</li>
+        <li class="p-2">- Obavezno platiti premiju osiguranja pre početka putovanja.</li>
+        <li class="p-2">- Obavezno pozvati call centar u slučaju potrebe. Lakše ćete kasnije naplatiti štetu.</li>
+        <li class="p-2">- Za vreme boravka nosite polisu stalno sa sobom.</li>
+    </ul>
 
 
 @endsection('page_data')
