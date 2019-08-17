86829ca035eba79196d58fa5340fcc4da4b05eda
Localhost user <tomicawarr@gmail.com>
2019-08-17 15:13:08 +0200

    *Image compressed

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,6 +2,18 @@
 
 <html>
     <head>
+
+        <meta name="google-site-verification" content="jp1tOjgQiV2-EtGGcfvq0roIIQJylf-w57FLrB3kSDM" />
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-145851598-1"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-145851598-1');
+        </script>
+
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta charset="UTF-8">
         <script src="/javascript/jquery.min.js"></script>
