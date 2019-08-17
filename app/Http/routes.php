af00f41a72feaae9ea2f39f258fe85c0faf9827a
Localhost user <tomicawarr@gmail.com>
2019-08-17 19:56:54 +0200

    routes for pages

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -16,11 +16,105 @@ if(version_compare(PHP_VERSION, '7.2.0', '>=')) {
 require __DIR__.'/admin_routes.php';
 
 Route::get('/', function () {
-    return view('pages/homepage');
+
+    $data = [
+        'O nama' => [
+            'url' => 'o-nama',
+            'image' => 'about.jpg'
+        ],
+        'krediti' => [
+            'url' => 'krediti',
+            'image' => 'loan.jpg'
+        ],
+        'osiguranje' => [
+            'url' => 'osiguranje',
+            'image' => 'deal.jpg'
+        ],
+        'fondovi' => [
+            'url' => 'fondovi',
+            'image' => 'fonds.jpg'
+        ],
+        'consulting' => [
+            'url' => 'consulting',
+            'image' => 'consulting.jpg'
+        ],
+        'auto' => [
+            'url' => 'auto',
+            'image' => 'car.jpg'
+        ],
+        'education' => [
+            'url' => 'edukacije',
+            'image' => 'education.jpg'
+        ],
+        'web' => [
+            'url' => 'web',
+            'image' => 'web.jpg'
+        ],
+        'loyalti' => [
+            'url' => 'loyalti',
+            'image' => 'loyalti.jpg'
+        ],
+
+    ];
+
+
+    return view('pages/homepage', ['data' => $data]);
 });
 
 Route::get('/kontakt', function() {
     return view('pages/contact');
+});
+
+Route::get('krediti', function() {
+   $data = [
+       'keš krediti' => [
+           'url' => 'kes-krediti',
+           'image' => 'loan.jpg'
+       ],
+       'refinansirajući' => [
+           'url' => 'refinansirajuci',
+           'image' => 'loan.jpg'
+       ],
+       'auto krediti' => [
+           'url' => 'auto-krediti',
+           'image' => 'loan.jpg'
+       ],
+       'stambeni krediti' => [
+           'url' => 'stambeni-krediti',
+           'image' => 'houseCredit.jpg'
+       ],
+       'potrošački krediti' => [
+           'url' => 'potrosacki-krediti',
+           'image' => 'consumerCredit.jpg'
+       ],
+   ];
+   return view('pages/homepage', ['data' => $data]);
+});
+
+Route::get('osiguranje', function() {
+    $data = [
+        'putno osiguranje' => [
+            'url' => 'putno-osiguranje',
+            'image' => 'travel.jpg'
+        ],
+        'osiguranje od ao' => [
+            'url' => 'osiguranje-od-ao',
+            'image' => 'carInsurance.jpg'
+        ],
+        'kasko osiguranje' => [
+            'url' => 'kasko-osiguranje',
+            'image' => 'carInsurance'
+        ],
+        'osiguranje imovine' => [
+            'url' => 'osiguranje-imovine',
+            'image' => 'house.jpg'
+        ],
+        'životno osiguranje' => [
+            'url' => 'zivotno-osiguranje',
+            'image' => 'life.jpg'
+        ],
+    ];
+    return view('pages/homepage', ['data' => $data]);
 });
 
 Route::any('/{any}', function($any) {
--- resources/views/pages/homepage.blade.php
+++ resources/views/pages/homepage.blade.php
@@ -6,55 +6,8 @@
 
 
         <article class="col-12 col-md-12 col-lg-9 d-flex flex-wrap justify-content-center m-auto bg-white overflow-hidden">
-            <a class="mt-lg-4 ml-lg-4 mr-lg-4 col-10 col-md-6 col-lg-3 list-group-item w-100 d-flex flex-column nounderline borderNone hoverWhite p-lg-0" href="#">
-                <img class="w-100 h-auto" src="/images/about.jpg" alt="image" />
-                <p class="backgroundBlue colorWhite p-2 pl-4 fontWeight600">O NAMA</p>
-            </a>
-
-            <a class="mt-lg-4 ml-lg-4 mr-lg-4 col-10 col-md-6 col-lg-3 list-group-item w-100 d-flex flex-column nounderline borderNone hoverWhite p-lg-0" href="#">
-                <img class="w-100 h-auto" src="/images/loan.jpg" alt="image" />
-                <p class="backgroundBlue colorWhite p-2 pl-4 fontWeight600">KREDITI</p>
-            </a>
-
-            <a class="mt-lg-4 ml-lg-4 mr-lg-4 col-10 col-md-6 col-lg-3 list-group-item w-100 d-flex flex-column nounderline borderNone hoverWhite p-lg-0" href="#">
-                <img class="w-100 h-auto" src="/images/deal.jpg" alt="image" />
-                <p class="backgroundBlue colorWhite p-2 pl-4 fontWeight600">OSIGURANJE</p>
-            </a>
-
-            <a class="mt-lg-4 ml-lg-4 mr-lg-4 col-10 col-md-6 col-lg-3 list-group-item w-100 d-flex flex-column nounderline borderNone hoverWhite p-lg-0" href="#">
-                <img class="w-100 h-auto" src="/images/fonds.jpg" alt="image" />
-                <p class="backgroundBlue colorWhite p-2 pl-4 fontWeight600">FONDOVI</p>
-            </a>
-
-            <a class="mt-lg-4 ml-lg-4 mr-lg-4 col-10 col-md-6 col-lg-3 list-group-item w-100 d-flex flex-column nounderline borderNone hoverWhite p-lg-0" href="#">
-                <img class="w-100 h-auto" src="/images/consulting.jpg" alt="image" />
-                <p class="backgroundBlue colorWhite p-2 pl-4 fontWeight600">CONSULTING</p>
-            </a>
-
-            <a class="mt-lg-4 ml-lg-4 mr-lg-4 col-10 col-md-6 col-lg-3 list-group-item w-100 d-flex flex-column nounderline borderNone hoverWhite p-lg-0" href="#">
-                <img class="w-100 h-auto" src="/images/car.jpg" alt="image" />
-                <p class="backgroundBlue colorWhite p-2 pl-4 fontWeight600">AUTO</p>
-            </a>
-
-            <a class="mt-lg-4 ml-lg-4 mr-lg-4 col-10 col-md-6 col-lg-3 list-group-item w-100 d-flex flex-column nounderline borderNone hoverWhite p-lg-0" href="#">
-                <img class="w-100 h-auto" src="/images/education.jpg" alt="image" />
-                <p class="backgroundBlue colorWhite p-2 pl-4 fontWeight600">EDUKACIJE</p>
-            </a>
-
-            <a class="mt-lg-4 ml-lg-4 mr-lg-4 col-10 col-md-6 col-lg-3 list-group-item w-100 d-flex flex-column nounderline borderNone hoverWhite p-lg-0" href="#">
-                <img class="w-100 h-auto" src="/images/web.jpg" alt="image" />
-                <p class="backgroundBlue colorWhite p-2 pl-4 fontWeight600">WEB</p>
-            </a>
-
-            <a class="mt-lg-4 ml-lg-4 mr-lg-4 col-10 col-md-6 col-lg-3 list-group-item w-100 d-flex flex-column nounderline borderNone hoverWhite p-lg-0" href="#">
-                <img class="w-100 h-auto" src="/images/loyalti.jpg" alt="image" />
-                <p class="backgroundBlue colorWhite p-2  pl-4 fontWeight600">LOYALTI</p>
-            </a>
-
+            @include("partials/homepage", ['data' => $data])
         </article>
-
-
-
     </section>
 
     <section class="container-fluid orangeBackground d-flex flex-row p-3 align-items-center justifyEvenly">
