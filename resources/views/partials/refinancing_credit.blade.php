7b86ef66226d09f6c140debf510cf12b1707b470
Localhost user <tomicawarr@gmail.com>
2019-09-02 16:39:58 +0200

    refinancing credits

---
+++ resources/views/partials/refinancing_credit.blade.php
@@ -0,0 +1,66 @@
+@extends('pages/permalink', ['page' => $page])
+
+
+@section('title')
+    <title>Refinansirajući krediti</title>
+@endsection
+
+@section('page_data')
+
+    <h1>Kredit za refinansiranje</h1>
+
+    <img class="col-12 col-lg-11 h-auto p-2" src="/images/cash.jpg" title="imege" />
+
+    <p>Šta je kredit za refinansiranje?</p>
+
+    <p>Kredit za refinansiranje je kredit koji podrazumeva zamenu postojećeg duga po osnovu jednog ili više dobijenih kredita novim kreditom,
+        obično u istom iznosu i sa istim sredstvima obezbeđenja, ali pod drugačijim uslovima (kamatna stopa, duži rok otplate, grejs period ili poček).
+        Uslovi odobravanja kredita za refinansiranje razlikuju se od banke do banke, tj. zavise od poslovne politike banke, i mogu biti promenljiva kategorija
+        , naročito u periodu otplate.
+    </p>
+
+    <p>Kreditom za refinansiranje možete da:</p>
+    <ul class="listNone p-1 p-lg-2">
+        <li class="p-2">- izvršite prevremenu otplatu kredita koje ste već iskoristili,</li>
+        <li class="p-2">- izmirite obaveze po kreditnim karticama,</li>
+        <li class="p-2">- izmirite obaveze po osnovu odobrenih i iskorišćenih prekoračenja po tekućim računima,</li>
+        <li class="p-2">- izvršite prevremenu otplatu preostalih rata lizing naknade, i </li>
+        <li class="p-2">- izmirite obaveze po aktiviranim datim jemstvima, tj. kao jemac otplatite nečiju obavezu.</li>
+    </ul>
+
+    <p>Refinansiranje kredita je isplativo samo ukoliko uslovi pod kojima je kredit za refinansiranje odobren snižavaju ukupnu cenu kredita i čine je nižom od cene postojećeg kredita.
+    </p>
+
+    <p>Kada je kredit za refinansiranje dobro rešenje?</p>
+
+    <p>Kredit za refinansiranje je dobro rešenje onda kada je kamatna stopa dovoljno niža od postojeće kamatne stope da obezbedi ukupno nižu cenu kredita.
+        Kredit za refinansiranje vam omogućava značajno smanjenje ukupnih troškova otplate kredita
+        (naročito ukoliko se refinansiranje izvrši na početku otplatnog perioda i kod dugoročnih kredita, tj. na pet i više godina) uz relativno malo povećanje mesečne rate.
+    </p>
+
+    <p>O čemu treba voditi računa pre nego što uzmete kredit za refinansiranje?</p>
+
+    <p>Ukoliko procenite da vam je neophodan kredit za refinansiranje:</p>
+    <ul class="listNone p-1 p-lg-2">
+        <li class="p-2">- Uporedite ponude različitih banaka, tj. pod kojim uslovima vam banke nude ovu vrstu kredita
+            (visinu nominalne kamatne stope, a naročito efektivne kamatne stope, dužinu perioda otplate, visinu mesečne rate,
+            kao i sredstva koja banke traže kao obezbeđenje otplate kredita, tj. menice, izjave žiranata, hipoteke i dr. );
+        </li>
+        <li class="p-2">- Uporedite visinu mesečne obaveze koju imate po osnovu kredita u otplati i onu koju ćete imati po kreditu za refinansiranje (manji anuitet ne znači uvek i da je kredit jeftiniji);</li>
+        <li class="p-2">- Sagledajte troškove koji nastaju prilikom refinansiranja kredita:</li>
+        <li class="p-4">- troškove prevremenog vraćanja starog kredita na preostali iznos glavnice, i
+        </li>
+        <li class="p-4">- troškove obrade novog kredita (često se pominju i kao manipulativni troškovi).</li>
+    </ul>
+
+    <p>Troškovi novog kredita koji služi za refinansiranje mogu da se pogledaju u obrascu koji se zove Pregled bitnih elemenata otplate kredita, koji su banke obavezne da vam daju na uvid pre realizacije kredita.</p>
+
+    <p>Postojeće zaduženje, odnosno njegov preostali iznos, kao i iznos potreban za otplatu postojećeg kredita, možete u svakom trenutku, na vaš zahtev, od banke dobiti – besplatno.</p>
+
+    <p>Preostali iznos zaduženja po glavnici svakog kredita redovno se ažurira u vašem Ličnom izveštaju Kreditnog biroa, koji možete dobiti na zahtev kod vaše banke ili Udruženja banaka po određenoj tarifi.</p>
+
+    <p>Navedeni podatak nalazi se u okviru tačke 3. Obaveze po kreditima, 3.1. Korišćenje kredita u koloni Ostatak duga u Ličnom izveštaju.</p>
+
+
+
+@endsection('page_data')
