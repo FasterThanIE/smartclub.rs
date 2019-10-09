ca63883858e91f2e39ec03dbdf3675d0eaa1ab4c
Localhost user <tomicawarr@gmail.com>
2019-10-09 23:46:00 +0200

    * Added phone to the personal company page

--- resources/views/la/companies/company_page.blade.php
+++ resources/views/la/companies/company_page.blade.php
@@ -12,6 +12,7 @@
             <h1>Informacije</h1>
             <p class="col-4">Naziv firme: {{ $data['name'] }}</p>
             <p class="col-4">Puno ime: {{ $data['full_name'] }}</p>
+            <p class="col-4">Telefon: {{ $data['phone'] }}</p>
             <p class="col-4">MB: {{ $data['mb'] }}</p>
             <p class="col-4">PIB: {{ $data['pib'] }}</p>
             <p class="col-4">Aktiva: {{ $data['active_funds'] }}</p>
