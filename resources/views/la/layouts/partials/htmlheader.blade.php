73f3e8dc02fd83d83d9e344d5cff85e96b70840a
Localhost user <tomicawarr@gmail.com>
2019-10-17 22:55:45 +0200

    * Removed un-necessary items & added bootstrap 4

--- resources/views/la/layouts/partials/htmlheader.blade.php
+++ resources/views/la/layouts/partials/htmlheader.blade.php
@@ -3,7 +3,7 @@
     <title>@hasSection('htmlheader_title')@yield('htmlheader_title') - @endif{{ LAConfigs::getByKey('sitename') }}</title>
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
     <!-- Bootstrap 3.3.4 -->
-    <link href="{{ asset('la-assets/css/bootstrap.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
     
     <link href="{{ asset('la-assets/css/font-awesome.min.css') }}" rel="stylesheet" type="text/css" />
     <!--<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />-->
